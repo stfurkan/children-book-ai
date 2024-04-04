7ee57f3fa83fc03d360d5bdc05e31c4d804c28b9
Sait Furkan Teke <35101659+stfurkan@users.noreply.github.com>
2024-04-03 17:58:19 -0700

    add carousel

--- components/Home/FeaturedBooks.tsx
+++ components/Home/FeaturedBooks.tsx
@@ -1,14 +1,29 @@
 import { BookCardType } from '@/types/dbTypes';
-import { BookCard } from '../Books/BookCard';
+import { BookCard } from '@/components/Books/BookCard';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export function FeaturedBooks({ allBooks }: { allBooks?: BookCardType[] }) {
   return (
     <section className="flex flex-col items-center py-12">
       <h2 className="font-mono text-2xl md:text-3xl font-bold text-center mb-8">~ Featured Books ~</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        {allBooks?.map((book) => (
-            <BookCard key={book.id} book={book} />
-        ))}
+      <div className="max-w-sm md:max-w-3xl px-4">
+        <Carousel>
+          <CarouselContent>
+            {allBooks?.map((book) => (
+                <CarouselItem key={book.id} className="basis-1/2 md:basis-1/3">
+                  <BookCard book={book} />
+                </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </section>
   );
